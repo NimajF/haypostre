95b0799605b990fdaf13abd62f155551b2af7ea0
NimajF <benjafran2000@gmail.com>
2023-05-08 15:55:00 -0300

    Add how-to-buy info

---
+++ src/components/HowToBuy.js
@@ -0,0 +1,40 @@
+import { BsBoxSeam } from "react-icons/bs";
+import { RiWhatsappLine } from "react-icons/ri";
+import { TbPuzzle } from "react-icons/tb";
+import styles from "../styles/Home.module.css";
+
+export default function HowToBuy() {
+  return (
+    <div className={styles.howToBuyDiv}>
+      <h4>¿Cómo comprar?</h4>
+      <div className={styles.howToBuyInfo}>
+        <div className={styles.infoBox}>
+          {" "}
+          <BsBoxSeam />
+          <p>
+            Para realizar un pedido es tan fácil como agregar al carrito y
+            clickear en comprar. El pedido se reserva con una seña del 50% la
+            cual se descontará del total.
+          </p>
+        </div>
+        <div className={styles.infoBox}>
+          {" "}
+          <TbPuzzle />
+          <p>
+            Al momento de finalizar el pedido, podrás envíar comentarios para
+            terminar de personalizar las características del mismo y de sus
+            productos
+          </p>
+        </div>
+        <div className={styles.infoBox}>
+          <RiWhatsappLine />
+          <p>
+            Luego podrás mandarme un mensaje por WhatsApp. Una vez enviado el
+            pedido me pondré en contacto para coordinar métodos de pago y
+            dirección de envío.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Layout from "@/components/Layout";
 import ParallaxBg from "@/components/ParallaxBG";
 import ProductNav from "@/components/ProductNav";
+import HowToBuy from "@/components/HowToBuy";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -29,26 +30,7 @@ export default function Home() {
 
             <h2 style={{ textAlign: "center" }}></h2>
           </div>
-          <div className={styles.howToBuyDiv}>
-            <div className={styles.howToBuyInfo}>
-              <h4>¿Cómo comprar?</h4>
-              <ul>
-                <li>
-                  Para realizar un pedido es tan fácil como agregar al carrito y
-                  clickear en comprar. El pedido se reserva con una seña del 50%
-                  la cual se descontará del total.
-                </li>
-                <li>
-                  Luego podrás mandarme un mensaje por WhatsApp. Podrás también
-                  personalizar el pedido si es posible.
-                </li>
-                <li>
-                  Una vez enviado el pedido me pondré en contacto para coordinar
-                  métodos de pago y dirección de envío.
-                </li>
-              </ul>
-            </div>
-          </div>
+          <HowToBuy />
         </main>
       </Layout>
     </>

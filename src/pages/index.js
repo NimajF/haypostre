18814cb438a0ce58f9141b2f120d9750ae581e94
NimajF <benjafran2000@gmail.com>
2023-08-23 15:48:33 -0300

    Delete index page default favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
           content="Pastelería artesanal Rosario. Envíos a domicilio"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Layout>
         <main className={styles.main}>
